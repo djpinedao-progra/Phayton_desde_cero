9de2a68d41dcaf21dbcc6556686d555711da8ef3
Dennis Pineda <djpinedao@alumno.uspg.edu.gt>
2026-02-14 17:30:20 -0600

    codigo sin bug

--- código con bug.py
+++ código con bug.py
@@ -1,14 +1,13 @@
 # Programa para validar si una persona es mayor de edad
 # Contiene un BUG intencional para que lo identifiques
 
-edad = input("Ingresa tu edad: ")
+edad = int (input("Ingresa tu edad: "))
 
 # Agregar prints para investigar
 print(f"DEBUG tipo: {type(edad)}")
-print(f"DEBUG valor: [{edad}]")
-print(f"DEBUG len: {len(edad)}")
+print(f"DEBUG valor: {edad}")
 
-if edad >= 18:  # Bug aquí!
+if edad >= 18:  # Bug arreglado!
     print("Eres mayor de edad")
 else:
     print("Eres menor de edad")
